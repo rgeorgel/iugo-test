c6579d3954a1dfe656206267e94c932b7d8155d0
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2019-12-05 09:27:36 -0800

    test to transaction

---
+++ test/api/transaction.spec.js
@@ -0,0 +1,79 @@
+const server = require('../../src/config/server');
+const supertest = require('supertest');
+const request = supertest(server);
+const Error = require('../../src/models/error');
+const mongoose = require('mongoose')
+
+beforeAll(async () => {
+  const url = `mongodb://iugo:iugo@localhost:27017/admin?ssl=false`;
+  await mongoose.connect(url, { useNewUrlParser: true });
+})
+
+describe('Transaction endpoint', () => {
+  test('Try to save transaction with invalid Verifier', async (done) => {
+    const transaction = {
+      "TransactionId": 2,
+      "UserId": 2,
+      "CurrencyAmount": 2,
+      "Verifier": "fd6b91387c2853ac8467bb4d90eac30897777fc6"
+    };
+
+    request.post('/Transaction')
+      .send(transaction)
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        const expectedError = new Error('The verifier code is invalid!');
+
+        expect(expectedError).toEqual(res.body);
+
+        done();
+      });
+  });
+
+  test('Save a valid transaction', async (done) => {
+    const transaction = {
+      "TransactionId": 1,
+      "UserId": 2,
+      "CurrencyAmount": 3,
+      "Verifier": "fd6b91387c2853ac8467bb4d90eac30897777fc6"
+    };
+
+    request.post('/Transaction')
+      .send(transaction)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        done();
+      });
+  });
+
+  test.skip('Try to Save a valid transaction duplicated', async (done) => {
+    const transaction = {
+      "TransactionId": 1,
+      "UserId": 2,
+      "CurrencyAmount": 3,
+      "Verifier": "fd6b91387c2853ac8467bb4d90eac30897777fc6"
+    };
+
+    request.post('/Transaction')
+      .send(transaction)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end();
+
+    request.post('/Transaction')
+      .send(transaction)
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        done();
+      });
+  });
+});
